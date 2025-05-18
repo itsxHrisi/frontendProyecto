69c0b9b8a216d74d1697c62c626005d14f2852a6
itsxHrisi <hristiyatg@gmail.com>
2025-05-18 15:59:37 +0300

    Commit footer y perfil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "./componentes_global/footer/footer.component";
 import { Footer2Component } from "./componentes_global/footer2/footer2.component";
 
 import { HeaderComponent } from "./componentes_global/header/header.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,Footer2Component],
+  imports: [RouterOutlet,HeaderComponent,Footer2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,27 @@
+// src/app/app.config.ts
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-
 import {
   provideRouter,
   withComponentInputBinding,
   withHashLocation,
+  withInMemoryScrolling
 } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptor/auth.interceptor';
 
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation(), withComponentInputBinding()),
-    //Declaracion del interceptor para que por cada llamada introduzca token si existe
+    provideRouter(
+      routes,
+      withHashLocation(),
+      withComponentInputBinding(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled', // ← vuelve siempre al top
+        anchorScrolling: 'enabled'            // ← opcional, para enlaces con “#ancla”
+      })
+    ),
     provideHttpClient(withInterceptors([authInterceptor])),
-    
   ],
-  
 };
