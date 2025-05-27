9459d592267887f5750bd84e308e55e123785cd4
itsxHrisi <122966420+itsxHrisi@users.noreply.github.com>
2025-05-27 22:56:40 +0200

    cambios enviroment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+//Rutas para produccion
+export const environment = {
+  production: false,
+  authUrl: 'http://localhost:8091/auth',
+  apiUrl: 'http://localhost:8091/api',
+};
--- src/environments/environment.ts
+++
@@ -1,2 +0,0 @@
-//Rutas para produccion
-export const environment = {};
